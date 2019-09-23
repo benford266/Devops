185fef8c3518022a63638136a0f2ab3f85ce1ace
benjamin ford <benjaminford@benjamins-mbp.home>
2019-09-23 19:20:00 +0100

    Cisco Port changer added

---
+++ manipulatestrings.py
@@ -0,0 +1,16 @@
+#using quotes in string, you need to just double quotes if using single quotes
+escapevalue = "Hello ben's cat"
+
+
+print(escapevalue)
+
+#You can also use escape characters to allow it.
+
+print("You can also use escape chars like backslash")
+
+escapechar = 'Hello bens\'s cat'
+
+print(escapechar)
+
+
+#we can also 
---
+++ portchanger.py
@@ -0,0 +1,63 @@
+## mac address search
+
+
+print("****** Bens Port Changer ******")
+ 
+## Get mac addresses from user
+maclist = []
+maccount = ''
+while True:
+  if maccount == '':
+    print("Enter Mac Address(return empty line to stop): ")
+    maccount = '1'  
+  macadd = input()
+  if macadd == '':
+    break
+  maclist = maclist + [macadd.lower()]
+ 
+## Clean up list of mac addresses
+ 
+cleanlist = [mac.replace(":","") for mac in maclist]
+ 
+## Split to last 4 chars
+splitlist = [split[-4:] for split in cleanlist]
+ 
+
+#print("Below are the mac addresses")
+#print(maclist)
+ 
+#print("Below are the removed chars")
+#print(cleanlist)
+ 
+## Print out Cisco commands
+print("Below are the commands to run in Cisco")
+for split in splitlist:
+  print("show mac add | inc {}".format(split))
+ 
+## Get switch info from UserWarning
+switchout = []
+configcount = ''
+while True:
+    if configcount == '':
+        print("\n\n\nEnter Switch output (return empty line to stop): ")
+        configcount = '1'
+    switchline = input()
+    if switchline  == '':
+        break
+    switchout = switchout + [switchline]
+ 
+interfacelist = []
+ 
+for l in switchout:
+  if ("Po" or "Gi" or "Fa") in l:
+    interface = l[-8:]
+    interface = interface.replace (" ","")
+    interfacelist = interfacelist + [str(interface)]
+ 
+dvlan = input("Please enter data vlan: ")
+vvlan = input("Please enter voice vlan: ")
+ 
+
+for port in interfacelist:
+  print("int {} \n switchport access vlan {} \n switchport voice vlan {}".format(port,dvlan,vvlan))
+ 
