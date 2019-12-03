d7a1211af9cdebcc9a85a67aea5e96444d7636a6
Ben Ford <benford_266@hotmail.com>
2019-12-03 13:50:15 +0000

    testing

--- Python/PortChanger/capturefunct.py
+++ Python/PortChanger/capturefunct.py
@@ -5,7 +5,7 @@ iftocap = 'Local Area Connection'
 
 def capturecdp(interface):
     load_contrib('cdp')
-    cdppacket = sniff(iface=interface, count=1, filter="ether dst 01:00:0c:cc:cc:cc")
+    cdppacket = sniff(iface=interface, count=1, filter="ip")
     device = cdppacket[0]['CDPMsgDeviceID'].val.decode()
     port = cdppacket[0]['CDPMsgPortID'].iface.decode()
     list = [device, port]
